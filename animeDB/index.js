d24fdcf468794b8b10095e9a1dbf3b1b3a0474b6
Inderprit Singh <singhinderprit1992@gmail.com>
2021-02-12 20:58:24 -0500

    second commit

--- animeDB/index.js
+++ animeDB/index.js
@@ -10,7 +10,8 @@ const syncAndSeed = async () =>{
         DROP TABLE IF EXISTS "Anime"; 
         CREATE TABLE "Anime"(
             id INTEGER PRIMARY KEY,
-            name VARCHAR(100) NOT NULL
+            name VARCHAR(100) NOT NULL,
+            description VARCHAR(20000)
         ); 
 
         CREATE TABLE "Character"(
@@ -19,9 +20,9 @@ const syncAndSeed = async () =>{
             anime_id INTEGER REFERENCES "Anime"(id)
         );
 
-        INSERT INTO "Anime"(id, name) VALUES(1, 'One Piece');
-        INSERT INTO "Anime"(id, name) VALUES(2, 'Attack On Titan');
-        INSERT INTO "Anime"(id, name) VALUES(3, 'Jujutsu Kaisen');
+        INSERT INTO "Anime"(id, name,description) VALUES(1, 'One Piece', 'This anime is about a young boy named Monkey D. Luffy who has a desire for adventure. He follows the footsteps of his childhood idol who is known as "Red Haired" Shanks. The story takes place in the age of pirates where all pirates are aiming to find the treasure known as "One Piece" that was left by the king of the pirates "Gold D. ROGER".');
+        INSERT INTO "Anime"(id, name, description) VALUES(2, 'Attack On Titan', 'Civilization is left within 3 walls. Over 100 years ago humanity had been driven to the brink of extinction after the emergence of human-like giants called Titans appeared and started to kill and eat all humans. Fast forward to the present and humanity lives in "peace" while not worrying about the outside threat, but they soon will be in for a rude awakening that this "peace" they have blinded themselves with, in reality is a ticking time bomb.');
+        INSERT INTO "Anime"(id, name, description) VALUES(3, 'Jujutsu Kaisen', 'This is the story of a high school student Yuji Itadori, who has no choice but to join an organization of sorcerors in order to kill the most powerful curse in existance who is personified as a human named "Ryoma Sukuna". The reason he doesn''t have a choice is because he becomes the host of Sukuna so either he kills himself, or he helps the sorceror organization hunt down and find a way to kill Sukuna. ');
 
         INSERT INTO "Character"(id, name, anime_id) VALUES(1, 'Monkey D. Luffy',1);
         INSERT INTO "Character"(id, name, anime_id) VALUES(2, 'Eren Yeager',2);
--- server.js
+++ server.js
@@ -19,16 +19,66 @@ app.get('/', async(req, res, next)=>{
               <link rel='stylesheet' href='/public/styles.css'/>
             </head>
             <body>
-            <h1>Epic Anime List</h1>
+            <h1 class ="title">Epic Anime List</h1>
+            <img class= "onePiece" src="https://i.pinimg.com/originals/77/86/bd/7786bdded50f80f4e71979b281e0eb78.jpg">
+            <img class ="AOT" src = "https://c4.wallpaperflare.com/wallpaper/756/731/751/anime-attack-on-titan-eren-yeager-shingeki-no-kyojin-wallpaper-thumb.jpg">
+            <img class ="jujutsu" src = "https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/4-jujutsu-kaisen-jonathan-dat.jpg">
+            <div class ="container1">
             <h2>Top 3 Anime</h2> 
             <ul>
             ${
                 animes.map(anime => `
-                    <li>
-                        <a href="/brands/${anime.id}"> 
+                    <li class="animes">
+                        <a href="/animes/${anime.id}"> 
                     ${anime.name}
                     </li>
+                    <a/>
+                    <p>${anime.description}</p>
                 `).join("")
+            }
+            </ul>
+            </div> 
+            </body>
+            </html>
+        `)
+
+    }catch(ex){
+        next(ex); 
+    }
+})
+
+app.get('/animes/:id', async(req, res, next)=>{
+    try{
+
+        const promises = [
+            client.query(`SELECT * FROM "Anime" WHERE id=$1;`, [req.params.id]), client.query(`SELECT * FROM "Character" WHERE anime_id=$1;`, [req.params.id])
+        ]
+        
+        const responses = await Promise.all(promises); 
+
+        //If you wanted to destructure and use it would be as follows
+        // const [animeResponse, charactersResponse] = await Promise.all(promises);
+        // const anime = animeResponse.rows[0];
+        // const characters = charactersResponse[1].rows; 
+
+        const anime = responses[0].rows[0];
+        
+        const characters = responses[1].rows; 
+
+        res.send(`
+            <html>
+            <head>
+              <link rel='stylesheet' href='/public/styles.css'/>
+            </head>
+            <body>
+            <h1>Epic Anime List</h1>
+            <h2><a href = "/">Top 3 Anime</a> <p>${anime.name}</p></h2>  
+            <ul>
+            ${
+                characters.map(character => `
+                <li class="characters">
+                  ${character.name}
+                </li>`).join("")
             }
             </ul>
             </body>
